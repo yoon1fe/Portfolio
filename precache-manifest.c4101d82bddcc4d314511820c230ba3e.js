ec3caf17631111f648a59e8e6974f09c03e27cf7
yoon1fe <yoon1fe@naver.com>
2020-06-09 01:19:37 +0900

    Updates

---
+++ precache-manifest.c4101d82bddcc4d314511820c230ba3e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1a8689c70572e5fa696c376e834487",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "650bef1f0cd35c2b4eee",
+    "url": "/portfolio/static/css/main.299b7c96.chunk.css"
+  },
+  {
+    "revision": "be7edfc3f6d60af00621",
+    "url": "/portfolio/static/js/2.bfa85a62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.bfa85a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650bef1f0cd35c2b4eee",
+    "url": "/portfolio/static/js/main.f4195f02.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "2acf3a70e37bc848995c5f4c7a3bb1d1",
+    "url": "/portfolio/static/media/bg.2acf3a70.jpg"
+  },
+  {
+    "revision": "93cadd83a7d469851b695089977f93cb",
+    "url": "/portfolio/static/media/img.93cadd83.png"
+  }
+]);
